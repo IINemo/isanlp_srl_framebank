2441b8816d69bbd704ef3f53e029cc6946164add
tchewik <elenachistov@gmail.com>
2020-04-17 17:38:52 +0000

    role filtering

--- src/isanlp_srl_framebank/processor_srl_framebank.py
+++ src/isanlp_srl_framebank/processor_srl_framebank.py
@@ -1,28 +1,19 @@
-from tensorflow.python.keras.models import load_model
-from tensorflow.python.keras.backend import set_session
-from gensim.models import KeyedVectors
-from deeppavlov.models.embedders.elmo_embedder import ELMoEmbedder
-from scipy.optimize import linear_sum_assignment
-from sklearn.preprocessing import LabelBinarizer
-from bert_serving.client import BertClient
+import json
+import logging
+import os
+import pickle
 
 import numpy as np
-import pickle
-import json
-import os
-from collections import namedtuple
-import sys
-
-import tensorflow as tf
 import tensorflow.lite as tfl
+from bert_serving.client import BertClient
+from deeppavlov.models.embedders.elmo_embedder import ELMoEmbedder
+from gensim.models import KeyedVectors
+from isanlp.annotation import Event, TaggedSpan
+from scipy.optimize import linear_sum_assignment
 
 from .argument_extractor import ArgumentExtractor
 from .predicate_extractor import PredicateExtractor
 from .preposition_extract import extract_preposition
-
-from isanlp.annotation import Event, TaggedSpan
-
-import logging
 
 logger = logging.getLogger('isanlp_srl_framebank')
 logging.basicConfig(filename="sample.log", level=logging.INFO)
@@ -31,26 +22,22 @@ logging.basicConfig(filename="sample.log", level=logging.INFO)
 # TODO: limit role confidence
 # TODO: fix logging
 # TODO: Evaluation script for parser (in addition to evaluation of models)
-# TOOD: more features for predicate
+# TODO: more features for predicate
 # TODO: non-core roles
 # TODO: refactor
-
-# select type of embeddings for each model here ('w2v' | 'elmo' | 'bert')
-# _KNOWN_PREDS_EMBEDDINGS = 'elmo'
-# _UNKNOWN_PREDS_EMBEDDINGS = 'elmo'
 
 
 class FeatureModelDefault:
     def extract_features(self, pred, arg, postag,
                          morph, lemma, syntax_dep_tree):
-        
+
         def prepos_lemma(position):
             if not position:
                 return ''
             if type(position) == int:
                 return lemma[position]
             return '~'.join([lemma[e] for e in position])
-            
+
         arg_pos = postag[arg]
         arg_case = morph[arg].get('Case', '')
         pred_pos = postag[pred]
@@ -100,15 +87,15 @@ class FeatureModelUnknownPredicates:
 class ModelProcessorSrlFramebank:
     def __init__(self, model_dir_path, embeddings_type):
         self._model_dir_path = model_dir_path
-        #-----------------------Using TFLite------------------------
+        # -----------------------Using TFLite------------------------
         logger.info('Loading the model...')
         self._model = tfl.Interpreter(os.path.join(model_dir_path, 'neural_model.tflite'))
         logger.info('Done.')
         self._model.allocate_tensors()
-        self._input_idxs = {x['name']:x['index'] for x in self._model.get_input_details()}
+        self._input_idxs = {x['name']: x['index'] for x in self._model.get_input_details()}
         self._output_idx = self._model.get_output_details()[0]['index']
-        #-----------------------------------------------------------
-        
+        # -----------------------------------------------------------
+
         logger.info('Loading embeddings...')
         self._embeddings_type = embeddings_type
         self._embeddings = self._load_embeddings()
@@ -124,16 +111,16 @@ class ModelProcessorSrlFramebank:
         with open(os.path.join(model_dir_path, 'feature_model.pckl'), 'rb') as f:
             self._feature_model = pickle.load(f)
         logger.info('Done.')
-        
+
     def _load_embeddings(self):
         if type(self._model_dir_path) == tuple:
             self._model_dir_path = self._model_dir_path[0]
-            
+
         if self._embeddings_type == 'w2v':
             embedding_path = os.path.join(self._model_dir_path, 'embeddings.vec')
             logger.info(f'Model has no embeddings! Loading {embedding_path}')
             return KeyedVectors.load_word2vec_format(embedding_path, binary=False)
-        
+
         elif self._embeddings_type == 'elmo':
             embedding_path = "http://files.deeppavlov.ai/deeppavlov_data/elmo_ru-wiki_600k_steps.tar.gz"
             logger.info(f'Model has no embeddings! Loading {embedding_path}')
@@ -142,7 +129,7 @@ class ModelProcessorSrlFramebank:
         elif self._embeddings_type == 'bert':
             bert_host = os.environ['BERT_HOST']
             return BertClient(ip=bert_host)
-        
+
     def _vectorize_embeddings(self, feature_embeddings, sent_embeddings, position):
         def _w2v():
             results = []
@@ -153,35 +140,36 @@ class ModelProcessorSrlFramebank:
                     results.append(np.zeros((self._embeddings.vector_size,)))
 
             return np.concatenate(results)
-                                
+
         def _contextual():
-            return np.array(sent_embeddings[min(position, len(sent_embeddings)-1)])
-                                
+            return np.array(sent_embeddings[min(position, len(sent_embeddings) - 1)])
+
         if self._embeddings_type == 'w2v':
             return _w2v()
-        
+
         if self._embeddings_type in ['elmo', 'bert']:
             return _contextual()
-        
+
     def _embed_sentence(self, tokens):
         def _w2v():
             return []
-                                
+
         def _elmo():
             return self._embeddings([tokens])[0]
 
         def _bert():
             res = self._embeddings.encode([tokens], is_tokenized=True, show_tokens=False)
-            return res[0, 1:len(tokens)+1, :]
-        
+            return res[0, 1:len(tokens) + 1, :]
+
         if self._embeddings_type == 'w2v':
             return _w2v()
-        
+
         if self._embeddings_type == 'elmo':
             return _elmo()
 
         if self._embeddings_type == 'bert':
             return _bert()
+
 
 class ProcessorSrlFramebank:
     def __init__(self,
@@ -210,17 +198,17 @@ class ProcessorSrlFramebank:
     def init(self):
         if not self._model_known_preds:
             with open(os.path.join(self.model_dir_path, 'known_preds.json'), 'r', encoding='utf8') as f:
-                self._known_preds = set(json.load(f))
-                            
+                self._known_preds = json.load(f)  # set(json.load(f))
+
             logger.info('Loading the model for known predicates...')
-            self._model_known_preds = ModelProcessorSrlFramebank(os.path.join(self.model_dir_path, 'known_preds'), 
+            self._model_known_preds = ModelProcessorSrlFramebank(os.path.join(self.model_dir_path, 'known_preds'),
                                                                  embeddings_type=self._known_preds_embeddings_type)
             logger.info('Model for known predicates is loaded.')
 
         if not self._model_unknown_preds:
             path_model_unknown_preds = os.path.join(self.model_dir_path, 'unknown_preds')
             if self.enable_model_for_unknown_predicates and os.path.exists(path_model_unknown_preds):
-                self._model_unknown_preds = ModelProcessorSrlFramebank(path_model_unknown_preds, 
+                self._model_unknown_preds = ModelProcessorSrlFramebank(path_model_unknown_preds,
                                                                        embeddings_type=self._unknown_preds_embeddings_type)
                 logger.info('Model for unknown predicates is loaded.')
             else:
@@ -246,7 +234,7 @@ class ProcessorSrlFramebank:
             result.append(np.concatenate(vectorized_feats).reshape(1, -1))
 
         return result
-    
+
     def _apply_threshold(self, predictions, threshold):
         predictions[predictions < threshold] = 0.
         return np.array(predictions)
@@ -273,7 +261,7 @@ class ProcessorSrlFramebank:
         model._model.set_tensor(model._input_idxs['input_categorical'], np.array(plain_features, dtype=np.float32))
         model._model.invoke()
         res = model._model.get_tensor(model._output_idx)
-        
+
         res = self._apply_threshold(res, threshold=.1)
         logger.info('Done.')
 
@@ -289,14 +277,14 @@ class ProcessorSrlFramebank:
             sent_morph = morph[sent_num]
             sent_lemma = lemma[sent_num]
             sent_syntax_dep_tree = syntax_dep_tree[sent_num]
-            
-            if self._model_known_preds._embeddings_type in ('elmo' ,'bert'):
-                sent_tokens = [token.text for token in tokens[tokens_counter:tokens_counter+len(sent_postag)]]
+
+            if self._model_known_preds._embeddings_type in ('elmo', 'bert'):
+                sent_tokens = [token.text for token in tokens[tokens_counter:tokens_counter + len(sent_postag)]]
                 tokens_counter += tokens_counter + len(sent_postag)
                 sent_embeddings = self._model_known_preds._embed_sentence(sent_tokens)
             else:
                 sent_embeddings = [], []
-            
+
             preds = self._predicate_extractor(sent_postag, sent_morph,
                                               sent_lemma, sent_syntax_dep_tree)
 
@@ -305,7 +293,7 @@ class ProcessorSrlFramebank:
                 pred_arg_roles = []
 
                 pred_lemma = sent_lemma[pred]
-                if pred_lemma in self._known_preds or self._model_unknown_preds is None:
+                if pred_lemma in self._known_preds.keys() or self._model_unknown_preds is None:
                     model = self._model_known_preds
                     logger.info('Using model for known predicates.')
                 else:
@@ -325,17 +313,22 @@ class ProcessorSrlFramebank:
                                                         sent_lemma,
                                                         sent_syntax_dep_tree)
                                  for arg in args]
-                    
+
                     clean_args = []
                     clean_roles = []
                     for i, arg in enumerate(args):
-                        if model._roles.inverse_transform(arg_roles[i]) != 'non_rel':
+                        arg_role_name = model._roles.inverse_transform(arg_roles[i])
+                        if pred_lemma in self._known_preds.keys():
+                            if arg_role_name in self._known_preds.get(pred_lemma):
+                                clean_args.append(arg)
+                                clean_roles.append(arg_roles[i])
+                        elif arg_role_name != 'non_rel':
                             clean_args.append(arg)
                             clean_roles.append(arg_roles[i])
-                    
+
                     args = clean_args
                     arg_roles = clean_roles
-                    
+
                     if arg_roles:
 
                         if self._enable_global_scoring:
--- src/training/run_training_pipeline.py
+++ src/training/run_training_pipeline.py
@@ -43,6 +43,47 @@ def extract_known_predicates(features_path, workdir):
     with open(os.path.join(workdir, 'known_preds.json'), 'w') as f:
         json.dump(known_preds, f)
         
+def extract_known_predicated(cleared_corpus_path, workdir):
+    def make_pred_dict(data_chunk, pred_dict):
+        for sentence in data_chunk[1]:
+            for word in sentence:
+                pred_number = word.get('fillpred')
+                if pred_number:
+                    if not pred_dict.get(sentence[pred_number]['lemma']):
+                        pred_dict[sentence[pred_number]['lemma']] = {word.get('rolepred1'): 1}
+                    else:
+                        if not pred_dict.get(sentence[pred_number]['lemma']).get(word.get('rolepred1')):
+                            pred_dict[sentence[pred_number]['lemma']][word.get('rolepred1')] = 1
+                        else:
+                            pred_dict[sentence[pred_number]['lemma']][word.get('rolepred1')] += 1
+                            
+    def filter_roles(pred_dictionary, threshold=5):
+        filtered_dict = {}
+        for predicate in pred_dictionary.keys():
+            new_pred = {}
+            for (key, value) in pred_dictionary[predicate].items():
+                if value > threshold:
+                    new_pred[key] = value
+            filtered_dict[predicate] = new_pred
+
+        for predicate in filtered_dict.keys():
+            if not filtered_dict[predicate]:
+                top = sorted(pred_dict[predicate], key=pred_dict[predicate].get, reverse=True)[0]
+                filtered_dict[predicate][top] = pred_dict[predicate][top]
+
+        return filtered_dict
+    
+    with open(cleared_corpus_path, 'r') as f:
+        data = json.load(f)
+    
+    pred_dict = {}                            
+    for instance in data:
+        make_pred_dict(instance, pred_dict)
+        
+    pred_dict = filter_roles(pred_dict)
+    known_preds = {key: list(value.keys()) for key, value in pred_dictionary.items()}
+    with open(os.path.join(workdir, 'known_preds.json'), 'w') as f:
+        json.dump(known_preds, f)
     
 def main(data_dir, workdir):
     cleared_corpus_path = os.path.join(data_dir, 'cleared_corpus.json')
