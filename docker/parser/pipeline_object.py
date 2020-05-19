0c7978083911e0e539e9cf7f79e3f863c3876ee8
IINemo <ArtemShelmanov@gmail.com>
2020-05-19 12:33:33 +0300

    Fix threshold

--- docker/parser/pipeline_object.py
+++ docker/parser/pipeline_object.py
@@ -4,7 +4,8 @@ from isanlp import PipelineCommon
 PPL_SRL_FRAMEBANK = PipelineCommon([(ProcessorSrlFramebank('/models', 
                                                            enable_model_for_unknown_predicates=True, 
                                                            known_preds_embeddings_type='elmo',
-                                                           unknown_preds_embeddings_type='elmo'),
+                                                           unknown_preds_embeddings_type='elmo',
+                                                           threshold=0.6),
                                      ['tokens', 'postag', 'morph', 'lemma', 'syntax_dep_tree'],
                                      {0 : 'srl'})
                                    ],
